7934abb0b5e9b8204f4f2ce317e321ef98432ce8
akashr206 <akashr6514@gmail.com>
2024-10-05 21:35:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 
 
+// function for Adding a loading screen
+function addBouncer(container) {
+    let cont = document.querySelector(`.${container}`)
+    let bounce = document.createElement("div")
+    let loading = document.createElement("div")
+    loading.className = "loading"
+    bounce.className = "bounce"
+    cont.prepend(loading)
+    loading.prepend(bounce)
+}
+// function for removing the loading screen
+async function removeBouncer() {
+    async function expand() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve()
+            }, 400);
+        })
+    }
+    let loading = document.querySelector(".loading")
+    let ball = document.querySelector(".bounce")
+    ball.classList.remove("bounce")
+    ball.classList.add("bounce2")
+    await expand()
+    ball.classList.remove("bounce2")
+    loading.classList.remove("loading")
+
+}
 
 async function weatherData(city) {
     const APIKEY = "5d8227f1161e43a189d33628240210";
@@ -7,7 +35,7 @@ async function weatherData(city) {
     let response = await fetch(url)
     let wData = await response.json()
     for (const key in wData) {
-        if(key === "error"){
+        if (key === "error") {
             return wData.error.message
         }
     }
@@ -15,43 +43,45 @@ async function weatherData(city) {
 }
 
 async function main(city) {
-    let data = await weatherData(city)
 
-    if(typeof data ==="string"){
+    addBouncer("container")
+    let data = await weatherData(city)
+    removeBouncer()
+    if (typeof data === "string") {
         alert(data)
         return
     }
+    console.log(data.location.name);
+    if (city.toLowerCase() === data.location.name.toLowerCase()) {
+        let cName = document.querySelector("header h5")
+        let icon = document.querySelector(".conditionIcon img")
+        let condition = document.querySelector("#condition")
+        let temp = document.querySelector(".temp p span")
+        let rFeelpre = document.querySelector(".realFeelPre span")
+        let humiditypre = document.querySelector(".humidityPre span")
+        let wSpeed = document.querySelector(".windSpeed span")
+        let wDirection = document.querySelector(".windSpeed .inCircle")
+        let humidity = document.querySelector(".humidity span")
+        let humidProgress = document.querySelector(".humidity .inCircle")
+        let pressure = document.querySelector(".pressure span")
+        let rFeel = document.querySelector(".realFeel span")
 
-    let cName = document.querySelector("header h5")
-    let icon = document.querySelector(".conditionIcon img")
-    let condition = document.querySelector("#condition")
-    let temp = document.querySelector(".temp p span")
-    let rFeelpre = document.querySelector(".realFeelPre span")
-    let humiditypre = document.querySelector(".humidityPre span")
-    let wSpeed = document.querySelector(".windSpeed span")
-    let wDirection = document.querySelector(".windSpeed .inCircle")
-    let humidity = document.querySelector(".humidity span")
-    let humidProgress = document.querySelector(".humidity .inCircle")
-    let pressure = document.querySelector(".pressure span")
-    let rFeel = document.querySelector(".realFeel span")
-
-    console.log(data);
-    icon.setAttribute("src", data.current.condition.icon)
-    condition.innerHTML = data.current.condition.text
-    temp.textContent = Math.floor(data.current.temp_c)
-    cName.textContent = data.location.name
-    rFeelpre.textContent = Math.floor(data.current.feelslike_c)
-    humiditypre.textContent = data.current.humidity
-    wSpeed.textContent = data.current.wind_kph
-    wDirection.setAttribute("style",`transform: rotate(${data.current.wind_degree}deg)`)
-    humidity.textContent = data.current.humidity
-    humidProgress.setAttribute("style",`width: ${data.current.humidity}%;height: ${data.current.humidity}%`)
-    pressure.textContent = data.current.pressure_mb
-    rFeel.textContent = Math.floor(data.current.feelslike_c)
-    console.log(data.current.wind_degree);
-
-
-}   
+        icon.setAttribute("src", data.current.condition.icon)
+        condition.innerHTML = data.current.condition.text
+        temp.textContent = Math.floor(data.current.temp_c)
+        cName.textContent = data.location.name
+        rFeelpre.textContent = Math.floor(data.current.feelslike_c)
+        humiditypre.textContent = data.current.humidity
+        wSpeed.textContent = data.current.wind_kph
+        wDirection.setAttribute("style", `transform: rotate(${data.current.wind_degree}deg)`)
+        humidity.textContent = data.current.humidity
+        humidProgress.setAttribute("style", `width: ${data.current.humidity}%;height: ${data.current.humidity}%`)
+        pressure.textContent = data.current.pressure_mb
+        rFeel.textContent = Math.floor(data.current.feelslike_c)
+    }else{
+        alert("Make sure that you have entered a city name")
+    }
+}
 
 let searchbar = document.querySelector(".searchbar")
 
@@ -62,6 +92,7 @@ searchbar.addEventListener("keypress", (e) => {
     }
     return
 })
+
 
 
 
